e039bceead9c532316283afd5579c171c3fe4f90
Arter Tendean <arter.tendean.07@gmail.com>
2020-09-07 13:32:48 +0800

    Create AnimeMoeUs Twitter Bot

---
+++ bot.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import tweepy
+import time
+import random
+import secrets
+
+def get_data():
+    response = requests.get('https://api.animemoe.us/quotes/random/').text
+    response = json.loads(response)
+    return response
+
+def create_status():
+    #get twitter login session
+    auth = tweepy.OAuthHandler(secrets.alpha, secrets.beta)
+    auth.set_access_token(secrets.gamma, secrets.delta)
+    api = tweepy.API(auth)
+
+    data = get_data()
+
+    #create tweet
+    try:
+        print('Creating tweet.')
+        if len(data['text']) >= 280:
+            print('Tweet needs to be a bit shorter.')
+        else:
+            text = data['text']+'\n\n~'+data['author']+', '+data['source']
+            api.update_status(text)
+            print(text)
+    except:
+        print('Something wrong.')
+
+
+if __name__ == '__main__':
+    create_status()
